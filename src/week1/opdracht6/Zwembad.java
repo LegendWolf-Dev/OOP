c15eb509aef5413cd8a28112946d4c1272b23e5e
LegendWolf-Dev <jochem.nieuwenhuizen@student.hu.nl>
2022-02-10 14:02:15 +0100

    Practicum 2

---
+++ src/week1/opdracht6/Zwembad.java
@@ -0,0 +1,32 @@
+package week1.opdracht6;
+
+
+public class Zwembad {
+    private double Breedte;
+    private double Lengte;
+    private double Diepte;
+    public double inhoud;
+
+    public Zwembad(double Breedte,double Lengte,double Diepte){
+
+    }
+
+    public double getBreedte(){
+        return Breedte;
+    }
+    public double getLengte(){
+        return Lengte;
+    }
+    public double getDiepte(){
+        return Diepte;
+    }
+    public double setBreedte(){
+        return
+    }
+    public double setLengte(){
+        return
+    }
+    public double setDiepte(){
+        return
+    }
+}
