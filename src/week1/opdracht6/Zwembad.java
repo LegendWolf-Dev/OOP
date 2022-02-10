79960e22f92084729a68b2ae581edc34fc173ab4
LegendWolf-Dev <jochem.nieuwenhuizen@student.hu.nl>
2022-02-10 14:42:03 +0100

    Practicum 2 (Upadated)

--- src/week1/opdracht6/Zwembad.java
+++ src/week1/opdracht6/Zwembad.java
@@ -1,15 +1,22 @@
 package week1.opdracht6;
 
-
 public class Zwembad {
     private double Breedte;
     private double Lengte;
     private double Diepte;
     public double inhoud;
 
-    public Zwembad(double Breedte,double Lengte,double Diepte){
-
+        public Zwembad(double Breedte,double Lengte,double Diepte){
+            this.Breedte = Breedte;
+            this.Lengte = Lengte;
+            this.Diepte = Diepte;
     }
+    public Zwembad(){
+    Breedte = 0;
+    Lengte = 0;
+    Diepte = 0;
+    }
+
 
     public double getBreedte(){
         return Breedte;
@@ -20,13 +27,20 @@ public class Zwembad {
     public double getDiepte(){
         return Diepte;
     }
-    public double setBreedte(){
-        return
+    public void setBreedte(double x){
+        Breedte = x;
     }
-    public double setLengte(){
-        return
+    public void setLengte(double y){
+        Lengte = y;
     }
-    public double setDiepte(){
-        return
+    public void setDiepte(double z){
+        Diepte = z;
+    }
+    public double inhoud(){
+            inhoud = Lengte * Breedte * Diepte;
+            return inhoud;
+    }
+    public String toString(){
+           return ("Dit zwembad is " + Breedte + " meter breed, " + Lengte + " meter lang, en " + Diepte + " meter diep.");
     }
 }
